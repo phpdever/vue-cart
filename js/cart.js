19b51d25016ee482a907fa28d585630a0222bb9c
phpdever <510438833@qq.com>
2017-04-08 08:23:38 +0800

    vue.js购物车实例

---
+++ js/cart.js
@@ -0,0 +1,24 @@
+/*develop shoping cart function*/
+vm = new Vue({
+	el:".container",
+	data:{
+		title:'hello vue',
+		productList:[],
+	},
+	filters:{
+		formatMoney:function(val){
+            return "￥" + val.toFixed(2);
+        }
+	},
+	mounted:function(){
+		this.cartView();
+	},
+	methods:{
+		cartView:function(){
+			var _this = this;
+			this.$http.get('data/cart.json',{'id':1}).then(function(res){
+				_this.productList = res.body.result.productList;
+			});
+		},
+	},
+});
